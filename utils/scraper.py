c2f7f1cc4de0f5e728fd19868026767f6be9aa91
blue-codes-yep <linzblue5@gmail.com>
2023-06-29 12:59:15 -0400

    Now gathering data using scrapingbee for now

--- utils/scraper.py
+++ utils/scraper.py
@@ -1,8 +1,9 @@
+from scrapingbee import ScrapingBeeClient
 from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
 from collections import defaultdict
 from dotenv import load_dotenv
-from itertools import cycle
-from tenacity import retry, stop_after_attempt, wait_exponential
+from urllib.parse import urlparse, parse_qs
 import pandas as pd
 import requests
 import os
@@ -12,6 +13,7 @@ load_dotenv()
 username = os.getenv("STEAM_USERNAME")
 password = os.getenv("STEAM_PASSWORD")
 sessionid = os.getenv("STEAM_LOGIN_SECURE")
+api_key = os.getenv("API_KEY")
 
 
 # Login to Steam - To get the cookies - To-Do
@@ -30,57 +32,73 @@ def login(page):
     page.wait_for_navigation()
 
 
-def get_item_links(page):
-    # Navigate to the market page
-    page.goto("https://steamcommunity.com/market/")
-    print(page.content())
-    # Get all item elements
-    item_elements = page.query_selector_all('.market_listing_row_link')
+def get_item_links(client):
+    # Send a GET request to the ScrapingBee API
+    response = client.get("https://steamcommunity.com/market/", params={"render_js": "true"})
 
-    # Extract item links
-    item_links = [item.get_attribute('href') for item in item_elements]
-    return item_links
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML from the response
+        soup = BeautifulSoup(response.text, 'html.parser')
 
+        # Get all item elements
+        item_elements = soup.select('.market_listing_row_link')
 
-def get_item_id(page, link):
-    # Define a list to store the item nameid and appid
-    item_nameid = [None]
-    appid = [None]
-
-    # Define a callback function for the route
-    def handle_route(route, request):
-        # Check if the request URL matches the expected pattern
-        if 'itemordershistogram' in request.url:
-            # Extract the item nameid from the URL
-            item_nameid[0] = request.url.split('item_nameid=')[1].split('&')[0]
-            
-        # Continue the request
-        route.continue_()
-
-    # Start network interception
-    page.route('**', handle_route)
-
-    # Navigate to the item page
-    page.goto(link)
-
-    
-    if '/listings/' in link:
-        appid[0] = link.split('/listings/')[1].split('/')[0]
+        # Extract item links
+        item_links = [item['href'] for item in item_elements]
+        return item_links
     else:
-        print(f"Failed to find '/listings/' in {link}")
-        print(appid[0])
+        print(f"Failed to fetch page: {response.text}")
+        return []
 
-    # Stop network interception
-    page.unroute('**')
 
+
+
+def get_item_id(link):
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+
+        # Define a list to store the item nameid and appid
+        item_nameid = [None]
+        appid = [None]
+
+        # Define a callback function for the route
+        def handle_route(route, request):
+            # Check if the request URL matches the expected pattern
+            if 'itemordershistogram' in request.url:
+                # Extract the item nameid from the URL
+                item_nameid[0] = request.url.split('item_nameid=')[1].split('&')[0]
+                
+            # Continue the request
+            route.continue_()
+
+        # Start network interception
+        page.route('**', handle_route)
+
+        # Navigate to the item page
+        page.goto(link)
+
+        if '/listings/' in link:
+            appid[0] = link.split('/listings/')[1].split('/')[0]
+        else:
+            print(f"Failed to find '/listings/' in {link}")
+            print(appid[0])
+
+        # Stop network interception
+        page.unroute('**')
+
+        browser.close()
+    print(f"Item nameid: {item_nameid[0]}, appid: {appid[0]}")
     return item_nameid[0], appid[0]
+
 
 
 def get_histogram_data(item_nameid, headers):
     
     # Define the histogram link
-    histogram_link = f"https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid={item_nameid[0]}&two_factor=0"
-
+    histogram_link = f"https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid={item_nameid}&two_factor=0"
+    print(f'histogram link: {histogram_link}, {item_nameid}')
     # Send a GET request to the histogram route with the headers
     response = requests.get(histogram_link, headers=headers)
 
@@ -106,28 +124,39 @@ def process_histogram(histogram_data):
     return processed_data
     
 
-def get_item_details(page):
-    name = page.locator('.market_listing_item_name').inner_text()
-    game = page.locator('.market_listing_game_name').inner_text()
-    item_type_element = page.locator('#largeiteminfo_item_type').inner_text()
+def get_item_details(client, link, headers):
+    # Send a GET request to the ScrapingBee API
+    response = client.get(link, params={"render_js": "true"})
 
-    # Get all elements with the class 'market_commodity_orders_header_promote'
-    elements = page.locator('.market_commodity_orders_header_promote')
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML from the response
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Extract the data from the elements
-    items_for_sale = elements.nth(0).inner_text()
-    sell_price = elements.nth(1).inner_text()
-    buy_requests = elements.nth(2).inner_text()
-    buy_price = elements.nth(3).inner_text()
+        # Extract the item details from the HTML
+        name = soup.select_one('.market_listing_item_name').text
+        game = soup.select_one('.market_listing_game_name').text
+        item_type_element = soup.select_one('#largeiteminfo_item_type').text
 
-    return name, game, item_type_element, items_for_sale, sell_price, buy_requests, buy_price
+        # Get all elements with the class 'market_commodity_orders_header_promote'
+        elements = soup.select('.market_commodity_orders_header_promote')
 
+        # Extract the data from the elements
+        items_for_sale = elements[0].text if len(elements) > 0 else None
+        sell_price = elements[1].text if len(elements) > 1 else None
+        buy_requests = elements[2].text if len(elements) > 2 else None
+        buy_price = elements[3].text if len(elements) > 3 else None
 
-def get_priceoverview_data(name,appid):
+        print(f"Name: {name}, Game: {game}, Item Type: {item_type_element}, Items for Sale: {items_for_sale}, Sell Price: {sell_price}, Buy Requests: {buy_requests}, Buy Price: {buy_price}")
+        return name, game, item_type_element, items_for_sale, sell_price, buy_requests, buy_price
+    else:
+        print(f"Failed to fetch page: {response.text}")
+        return None, None, None, None, None, None, None
+
+def get_priceoverview_data(name,appid, headers):
     # Navigate to the priceoverview route
     name_encoded = name.replace(' ', '%20').replace('&', '%26')
-    priceoverview_link = f"https://steamcommunity.com/market/priceoverview/?appid={appid[1]}&currency=1&market_hash_name={name_encoded}"
-
+    priceoverview_link = f"https://steamcommunity.com/market/priceoverview/?appid={appid}&currency=1&market_hash_name={name_encoded}"
     # Send a GET request to the priceoverview route
     response = requests.get(priceoverview_link)
 
@@ -139,7 +168,7 @@ def get_priceoverview_data(name,appid):
         lowest_price = priceoverview_data['lowest_price']
         volume = priceoverview_data['volume']
         median_price = priceoverview_data['median_price']
-
+        print(f"Lowest Price: {lowest_price}, Volume: {volume}, Median Price: {median_price}")
         return lowest_price, volume, median_price
     else:
         print(f"Failed to get priceoverview data for {name}")
@@ -149,8 +178,8 @@ def get_priceoverview_data(name,appid):
 def get_pricehistory_data(name, headers, appid):
     name_encoded = name.replace(' ', '%20').replace('&', '%26')
     print(name_encoded)
-    print(appid[1])
-    pricehistory_link = f"https://steamcommunity.com/market/pricehistory/?appid={appid[1]}&market_hash_name={name_encoded}"
+    print(appid)
+    pricehistory_link = f"https://steamcommunity.com/market/pricehistory/?appid={appid}&market_hash_name={name_encoded}"
 
     # Send a GET request to the pricehistory route with the headers
     response = requests.get(pricehistory_link, headers=headers)
@@ -204,60 +233,64 @@ def process_pricehistory_data(pricehistory_data):
     return processed_data
 
 
-def process_item_links(page, link):
+def process_item_links(client, link, sessionid):
     print(f"Processing link: {link}")
-    item_nameid = get_item_id(page, link)
-    appid= get_item_id(page,link)
-    # Define your sessionid cookie
-
-    # Define your headers
+    item_nameid, appid = get_item_id(link)
+    
     headers = {
         'Cookie': f'steamLoginSecure={sessionid}'
     }
 
-    # Navigate to the item page
-    page.goto(link)
+    # Send a GET request to the ScrapingBee API
+    response = client.get(link, params={"render_js": "true"}, headers=headers)
 
-    # Get item details
-    name, game, item_type_element, items_for_sale, sell_price, buy_requests, buy_price = get_item_details(page)
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML from the response
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Get priceoverview data
-    lowest_price, volume, median_price = get_priceoverview_data(name, item_nameid)
+        # Get item details
+        name, game, item_type_element, items_for_sale, sell_price, buy_requests, buy_price = get_item_details(client, link, headers)
 
-    # Get pricehistory data
-    pricehistory_data = get_pricehistory_data(name, headers, appid)
+        # Get priceoverview data
+        lowest_price, volume, median_price = get_priceoverview_data(name, appid, headers)
 
-    if pricehistory_data:
-        # Process pricehistory data
-        daily_data = process_pricehistory_data(pricehistory_data)
-    
-    # Get histogram data
-    histogram_data = get_histogram_data(item_nameid, headers)
+        # Get pricehistory data
+        pricehistory_data = get_pricehistory_data(name, headers, appid)
 
-    if histogram_data:
-        # Process histogram data
-        processed_data = process_histogram(histogram_data)
+        if pricehistory_data:
+            # Process pricehistory data
+            daily_data = process_pricehistory_data(pricehistory_data)
+        
+        # Get histogram data
+        histogram_data = get_histogram_data(item_nameid, headers)
 
-    
-    data = {
-        'Name': [name],
-        'Game': [game],
-        'Item Type': [item_type_element],
-        'Items for Sale': [items_for_sale],
-        'Sell Price': [sell_price],
-        'Buy Requests': [buy_requests],
-        'Buy Price': [buy_price],
-        'Lowest Price': [lowest_price],
-        'Volume': [volume],
-        'Median Price': [median_price],
-        'Daily Data': [daily_data],
-        'Histogram Data': [processed_data]
-    }
+        if histogram_data:
+            # Process histogram data
+            processed_data = process_histogram(histogram_data)
 
-    item_df, daily_df, processed_df = create_dataframes(name, data, daily_data, processed_data)
+        
+        data = {
+            'Name': [name],
+            'Game': [game],
+            'Item Type': [item_type_element],
+            'Items for Sale': [items_for_sale],
+            'Sell Price': [sell_price],
+            'Buy Requests': [buy_requests],
+            'Buy Price': [buy_price],
+            'Lowest Price': [lowest_price],
+            'Volume': [volume],
+            'Median Price': [median_price],
+            'Daily Data': [daily_data],
+            'Histogram Data': [processed_data]
+        }
 
-    return item_df, daily_df, processed_df
+        item_df, daily_df, processed_df = create_dataframes(name, data, daily_data, processed_data)
 
+        return item_df, daily_df, processed_df
+    else:
+        print(f"Failed to fetch page: {response.text}")
+        return None, None, None
 def create_dataframes(name, data, daily_data, processed_data):
     item_df = pd.DataFrame(data)
 
@@ -298,17 +331,38 @@ def clean_data(item_df, daily_df, processed_df):
     return item_df, daily_df, processed_df
 
 
-def process_link(page, link):
+def process_link(client, link, sessionid):
+    item_df, daily_df, processed_df = process_item_links(client, link, sessionid)
+    item_df, daily_df, processed_df = clean_data(item_df, daily_df, processed_df)
+    
+    return item_df, daily_df, processed_df
+
+def main(): 
+    # Initialize the ScrapingBee client
+    client = ScrapingBeeClient(api_key=api_key)
+    headers = {
+        'Cookie': f'steamLoginSecure={sessionid}'
+    }
+    # Get item links
+    item_links = get_item_links(client)
+    
     item_dfs = []
     daily_dfs = []
     processed_dfs = []
     
-    item_df, daily_df, processed_df = process_item_links(page, link)
-    item_df, daily_df, processed_df = clean_data(item_df, daily_df, processed_df)
-    
-    item_dfs.append(item_df)
-    daily_dfs.append(daily_df)
-    processed_dfs.append(processed_df)
+    print(f"Found {len(item_links)} items")
+    for link in item_links:
+        for _ in range(3):
+            try:
+                item_df, daily_df, processed_df = process_link(client, link, sessionid)
+                item_dfs.append(item_df)
+                daily_dfs.append(daily_df)
+                processed_dfs.append(processed_df)
+                break
+            except Exception as e:
+                print(f"Failed to process link {link} due to {e}")
+        else:
+            print(f"Failed to process link {link} after {3} retries")
 
     all_items_df = pd.concat(item_dfs, ignore_index=True)
     all_daily_df = pd.concat(daily_dfs, ignore_index=True)
@@ -317,52 +371,6 @@ def process_link(page, link):
     all_items_df.to_csv('items.csv', index=False)
     all_daily_df.to_csv('daily.csv', index=False)
     all_processed_df.to_csv('processed.csv', index=False)
-
-def main(): 
-    with sync_playwright() as p:
-        proxies = [
-            "54.237.145.145:80",
-            "103.114.219.115:3128"
-        ]
-        proxy_pool = cycle(proxies)
-        
-        proxy = next(proxy_pool)
-        browser = p.chromium.launch(proxy={'server': proxy})
-        context = browser.new_context()
-
-        # Clear cookies and permissions
-        context.clear_cookies()
-        context.clear_permissions()
-
-        page = context.new_page()
-
-        item_links = get_item_links(page)
-        
-        print(f"Found {len(item_links)} items")
-        for link in item_links:
-            for _ in range(3):
-                try:
-                    process_link(page, link)
-                    break
-                except Exception as e:
-                    print(f"Failed to process link {link} due to {e}")
-                    # Get the next proxy from the pool
-                    proxy = next(proxy_pool)
-                    # Close the current browser
-                    browser.close()
-                    # Launch a new browser with the new proxy
-                    browser = p.chromium.launch(proxy={"server": proxy})
-                    context = browser.new_context()
-
-                    # Clear cookies and permissions
-                    context.clear_cookies()
-                    context.clear_permissions()
-
-                    page = context.new_page()
-            else:
-                print(f"Failed to process link {link} after {3} retries")
-
-        browser.close()
 
 if __name__ == "__main__":
     main()
